a318d98291bad673496b46d14b2b17b51471b4b2
duweidawang <1762353200@qq.com>
2024-07-20 20:12:24 +0800

    初始化排行榜项目

---
+++ src/main/java/com/example/ranklist/RanklistApplication.java
@@ -0,0 +1,13 @@
+package com.example.ranklist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RanklistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RanklistApplication.class, args);
+	}
+
+}
